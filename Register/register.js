33962d534ef5aca33a9a4678818c9058020361a2
Duong Dang <duongdang@Duongs-MacBook-Pro.local>
2025-10-19 16:09:49 +0700

    update validate

--- Register/register.js
+++ Register/register.js
@@ -6,6 +6,10 @@ function Register() {
   const pass = document.getElementById("password").value;
   const re_pass = document.getElementById("re_password").value;
   let acc = JSON.parse(localStorage.getItem("username")) || [];
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  const passRegex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+  const numberRegex = /[0-9]/g;
 
   function Validate() {
     let isVal = true;
@@ -13,14 +17,33 @@ function Register() {
       alert("Chưa nhập email!!!");
       return (isVal = false);
     }
+
+    if (!emailRegex.test(email)) {
+      alert("Email ko đúng định dạng!!");
+      return (isVal = false);
+    }
     if (phone === "") {
       alert("Chưa nhập số điện thoại!!!");
       return (isVal = false);
     }
+
+    if (!numberRegex.test(phone)) {
+      alert("Số điện thoại không đúng đinh dạnh!!!");
+      return (isVal = false);
+    }
+
     if (pass === "") {
       alert("Chưa nhập mật khẩu!!!");
       return (isVal = false);
     }
+
+    if (!passRegex.test(pass)) {
+      alert(
+        "Mật khẩu phải có ký tự viết hoa, viết thường, số và ký tự đặc biệt"
+      );
+      return (isVal = false);
+    }
+
     if (re_pass !== pass) {
       alert("Mật khẩu không khớp!!!");
       return (isVal = false);
